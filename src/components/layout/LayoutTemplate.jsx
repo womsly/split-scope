882440a15ec08d8a44aad9d4422d6384c2192dde
Alex Shevlin <alexshevlin1@gmail.com>
2024-01-20 12:40:37 -0800

    remove sidebar from login page.

--- src/components/layout/LayoutTemplate.jsx
+++ src/components/layout/LayoutTemplate.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import classes from './LayoutTemplate.module.scss';
 import { motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom'
 import Sidebar from '../sidebar/Sidebar';
 
 function Layout({ children }) {
+  const location = useLocation();
   return (
     <motion.div className={classes.layoutWrapper}
         initial={{opacity: 0}}
         animate={{opacity: 1}}
         exit={{opacity: 0}}>
-      <Sidebar />
+      {location.pathname !== "/auth"? 
+        <Sidebar /> 
+      :
+        <></>
+      }
       <main className={classes.container}>
         {children}
       </main>
