c3e4289a05b7225c878e1ee31809313155442013
Alex Shevlin <alexshevlin1@gmail.com>
2024-01-21 15:54:51 -0800

    WIP project cards from github projects.

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,14 +1,41 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/layout/LayoutTemplate'
 import classes from './Project.module.scss'
 import { columns, cards } from './data'
+import axios from 'axios';
 
 function Project() {
   const [updatedCards, setUpdatedCards] = useState(cards);
 
+  const getRepos = async () => {
+    try {
+      // const orgRes = await axios.get(`https://api.github.com/users/a-shevlin/orgs`, {
+      //   headers: {
+      //     Authorization: `Bearer ${import.meta.env.VITE_APP_ACCESS_TOKEN}`
+      //   }
+      // })
+      let org = ""
+      // if (orgRes.status === 200) {
+      //   orgRes.data.forEach((userOrg) => {
+      //     appConsole.log(userOrg.id)
+      //   })
+      // }
+      const response = await axios.get(`https://api.github.com/orgs/${org}/projects`, {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_APP_ACCESS_TOKEN}`
+        }
+      });
+      const repositories = response.data;
+      appConsole.log(response);
+      appConsole.log(repositories);
+    } catch (e) {
+      appConsole.error(e);
+    }
+  }
+  
 
   useEffect(() => {
-
+    getRepos();
   }, [updatedCards])
 
 
