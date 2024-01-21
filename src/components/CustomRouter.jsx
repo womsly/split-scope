d17e360607ca7263da347db0407a107ca7863737
Alex Shevlin <alexshevlin1@gmail.com>
2024-01-20 16:25:29 -0800

    add AuthContext and ProtectedRoutes

--- src/components/CustomRouter.jsx
+++ src/components/CustomRouter.jsx
@@ -3,16 +3,23 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { Home, Project } from '../pages'
 import Auth from '../pages/auth/Auth';
+import { AuthContextProvider } from '../context/AuthContext';
+import ProtectedRoute from './ProtectedRoute';
 function CustomRouter() {
   const location = useLocation();
   return (
     <AnimatePresence>
-      {/* providers listed here to wrap routes */}
+      <AuthContextProvider>
+        {/* providers listed here to wrap routes */}
         <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path='/project' element={<Project />} />
           <Route path='/auth' element={<Auth />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path='/project' element={<Project />} />
+          </Route>
         </Routes>
+      </AuthContextProvider>
       {/* closing provider tags */}
     </AnimatePresence>
   )
