3534d6908e873a303022a9788c82c98ed0aceff7
Alex Shevlin <alexshevlin1@gmail.com>
2024-02-07 20:03:08 -0800

    update home using PageHeader component.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,32 +2,31 @@ import React from 'react'
 import Layout from '../../components/layout/LayoutTemplate'
 import { daysOfWeek } from './data'
 import classes from './Home.module.scss'
+import PageHeader from '../../components/PageHeader/PageHeader'
 
 
 function Home() {
   return (
     <Layout>
-      <h1>Home</h1> 
-      {/* or username */}
-      <p>Time worked this week: 0</p>
-      <br />
-      <div className={classes.project_container}>
-        <div className={classes.project_individual}>
-          <div>
-            <p>Project Name:</p>
-            <p>Time Worked:</p>
+      <PageHeader>
+        <div className={classes.project_container}>
+          <div className={classes.project_individual}>
+            <div>
+              <p>Project Name:</p>
+              <p>Time Worked:</p>
+            </div>
+            {daysOfWeek.map((item) => {
+              return (
+                <div>
+                  <p>{item}</p>
+                  <input type='text' name={`${item}_hours`} placeholder='0'/>
+                </div>
+              )
+            })}
           </div>
-          {daysOfWeek.map((item) => {
-            return (
-              <div>
-                <p>{item}</p>
-                <input type='text' name={`${item}_hours`} placeholder='0'/>
-              </div>
-            )
-          })}
-        </div>
 
-      </div>
+        </div>
+      </PageHeader>
     </Layout>
   )
 }
